25435b227024f4f26e660c5e4b3c7000e7dced3a
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-02-22 11:27:26 -0500

    make it better

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from decouple import config
+from discord.ext import commands
+
+# Add the token of the bot
+DISCORD_TOKEN = config("BOT_TOKEN")
+
+# Description and basic configuration
+description = "Un Bot de Ayuda para quien lo Necesite"
+prefix = "!"
+bot = commands.Bot(command_prefix=prefix, description=description)
+
+
+# Bot initial config
+@bot.event
+async def on_ready():
+    print(f"Initial as {bot.user}")
+
+
+# Load the extensions
+extensions = ["cogs.moderation", "cogs.dogs",
+              "cogs.cat_facts", "cogs.quotes", "cogs.avatar", "cogs.ping"]
+
+for i in extensions:
+    try:
+        bot.load_extension(i)
+    except Exception as e:
+        print("Ups a ocurrido un error")
+        print(e)
+
+# Running the bot
+bot.run(DISCORD_TOKEN)
